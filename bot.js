54ddc17fa02e3a957e67de008c812bd36d14704a
Pranav Kondapalli <pranavrk1606@gmail.com>
2021-05-01 19:33:37 +0530

    third

--- bot.js
+++ bot.js
@@ -2,6 +2,7 @@ console.log("Beep Boop");
 require('dotenv').config();
 //node bot.js
 
+const fetch = require("node-fetch");
 const Discord = require('discord.js');
 const client = new Discord.Client();
 client.login(process.env.BOT_TOKEN);
@@ -37,14 +38,19 @@ const replies = [
 
 //commands
 client.on('message', gotMessage);
-function gotMessage(msg) {
+async function gotMessage(msg) {
   if (msg.content === "|q") {
     const r = Math.floor(Math.random() * replies.length);
     msg.channel.send(replies[r]);
   }
   if (msg.content === "|help") {
     msg.channel.send("**Here's a list of all the commands** \n `''|q' To show random quotes for when you're bored` \n \n More will be added soon");
-  } else if (msg.content == "|gif") {
-    msg.channel.send("gif!");
+  }
+  if (msg.content === "|gif") {
+    let url = `https://g.tenor.com/v1/search?q=cat&key=${process.env.TENORKEY}&limit=8`;
+    let response = await fetch(url);
+    let json = await response.json();
+    console.log(json);
+    msg.channel.send(json.results[0].url);
   }
 }
